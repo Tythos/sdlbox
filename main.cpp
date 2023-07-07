8aa18ab09b06afb4ebf6be8427cd249372fe6c7f
Brian Kirkpatrick <code@tythos.net>
2023-07-06 18:30:41 -0700

    initial template

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-07-06
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+
+int main(int nArgs, char** vArgs) {
+    std::cout << "Farewell, cruel world!" << std::endl;
+    return 0;
+}
